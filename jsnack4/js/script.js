6836ee8bc41c47389ebe38f3f07fdc6c957b5149
vi_ser <vinceulisse@gmail.com>
2024-02-05 15:45:43 +0100

    Esercizio concluso

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -3,21 +3,30 @@ Snack 4
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 */
 
+const outputElement = document.querySelector("#output");
+
 let numberUser;
 
 
 let counter = 0;
 
+// stampo 3 numeri
 do {
     numberUser = Number(prompt("Inserisci un numero intero"));
+
+    // controllo che l'utente abbia inserito un valore valido
     if (isNaN(numberUser) || !Number.isInteger(numberUser)) {
         alert("Il valore inserito non è valido")
     }
+
     else {
+
+        // se il numero è pari lo stampo
         if (numberUser % 2 == 0) {
-            console.log(numberUser);
+            outputElement.innerHTML += `${numberUser}  `;
         } else {
-            console.log(numberUser + 1);
+            // se il numero è dispari stampo quello successivo
+            outputElement.innerHTML += `${numberUser + 1}  `;
         }
     }
 
