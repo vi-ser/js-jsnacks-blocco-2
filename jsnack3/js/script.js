1c7e8e50b0b97493bd8f708fcb51025d42205c2e
vi_ser <vinceulisse@gmail.com>
2024-02-05 15:08:11 +0100

    Snack 3 completato

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -3,6 +3,10 @@ Snack 3
 Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro. (Se iniziate con un array da 3 elementi e l'altro da 5, eseguite istruzioni sinchè non ne hanno entrambi 5
 */
 
+
+const outputDavideElement = document.querySelector("#output-davide");
+const outputDanieleElement = document.querySelector("#output-daniele");
+
 // creo i due array
 const davideTickets = ["Roberto", "Alessio", "Miriam", "Emanuele", "Christian", "Arianna"];
 const danieleTickets = ["Luca", "Salvatore", "Alan"];
@@ -13,15 +17,33 @@ while (davideTickets.length != danieleTickets.length) {
 
     const newTicket = prompt("Inserisci il nome dello studente per distribuire i ticket");
 
-    // SE Davide ha più ticket assegno il nuovo ticket a Daniele
-    if (davideTickets.length > danieleTickets.length) {
-        danieleTickets.push(newTicket);
+    // controllo che il campo non sia vuoto
+    if (newTicket === "") {
+        alert("Non hai inserito nulla!")
     }
-    // SE Daniele ha più ticket assegno il nuovo ticket a Davide
-    else if (davideTickets.length < danieleTickets.length) {
-        davideTickets.push(newTicket);
+
+    else {
+        // SE Davide ha più ticket assegno il nuovo ticket a Daniele
+        if (davideTickets.length > danieleTickets.length) {
+            danieleTickets.push(newTicket);
+        }
+        // SE Daniele ha più ticket assegno il nuovo ticket a Davide
+        else if (davideTickets.length < danieleTickets.length) {
+            davideTickets.push(newTicket);
+        }
     }
+
 }
 
-console.log(davideTickets, danieleTickets);
+// stampa dei due array
+for (let i = 0; i < davideTickets.length; i++) {
 
+    outputDavideElement.innerText = davideTickets.join(', ');
+
+}
+
+for (let j = 0; j < danieleTickets.length; j++) {
+
+    outputDanieleElement.innerText = danieleTickets.join(', ');
+
+}
