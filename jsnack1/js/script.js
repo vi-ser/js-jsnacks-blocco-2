9fe1fa578e7c814b6a17b5dc98e2d13b06ccbd71
vi_ser <vinceulisse@gmail.com>
2024-02-05 13:21:33 +0100

    logica snack 1

---
+++ jsnack1/js/script.js
@@ -0,0 +1,38 @@
+/*
+Snack 1
+Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
+*/
+
+// creo l'array vuoto
+const numbers = [];
+
+// inizializzo la variabile somma
+let sum = 0;
+
+// inizializzo la variabile del prompt
+let numberUser;
+
+// ciclo finché la somma dei numeri inseriti è minore di 50
+while (sum < 50) {
+
+    // controllo che il valore inserito sia un numero
+    do {
+        numberUser = Number(prompt("Inserisci un numero"));
+        if (isNaN(numberUser)) {
+            alert("Il valore inserito non è valido");
+        }
+    } while (isNaN(numberUser));
+
+    console.log(numberUser);
+
+    // incremento la somma del valore inserito dall'utente
+    sum += numberUser;
+
+    // se la somma non supera 50 aggiungo il numero inserito all'array
+    if (sum < 50) {
+        numbers.push(numberUser);
+    }
+
+}
+
+console.log(numbers);
